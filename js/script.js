3004084b5a67c70d7fad9f088277548e82b0709e
ardiona <ardionarahadi@gmail.com>
2024-09-14 20:27:55 +0700

    memperbarui tampilan login

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let check = document.getElementById("show-pw");
+let passwd = document.getElementById("password");
+check.addEventListener("click", () => {
+  if (passwd.type === "password") {
+    passwd.type = "text";
+  } else {
+    passwd.type = "password";
+  }
+});
