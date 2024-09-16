447865336a57780014589db3f93fdb610034fbc9
AlphaOmega <ardionabd08@gmail.com>
2024-09-16 09:15:49 +0700

    Memperbaiki bug pada collpase item  di sidebar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 let check = document.getElementById("show-pw");
 let passwd = document.getElementById("password");
 check.addEventListener("click", () => {
-  if (passwd.type === "password") {
-    passwd.type = "text";
-  } else {
-    passwd.type = "password";
-  }
+    if (passwd.type === "password") {
+        passwd.type = "text";
+    } else {
+        passwd.type = "password";
+    }
 });
