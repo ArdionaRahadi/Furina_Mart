8c2f2d8cd4540df59b153410db295c96cc01d018
ardiona <ardionarahadi@gmail.com>
2024-11-09 20:38:53 +0700

    refacoring part 4

--- backups/db_kasir.sql
+++ backups/db_kasir.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 09 Nov 2024 pada 07.30
--- Versi server: 11.5.2-MariaDB
--- Versi PHP: 8.3.10
+-- Host: localhost:3306
+-- Generation Time: Nov 09, 2024 at 01:30 PM
+-- Server version: 11.4.2-MariaDB-log
+-- PHP Version: 8.3.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `t_barang_keluar`
+-- Table structure for table `t_barang_keluar`
 --
 
 CREATE TABLE `t_barang_keluar` (
@@ -32,14 +32,13 @@ CREATE TABLE `t_barang_keluar` (
   `barcode` varchar(20) NOT NULL,
   `nama_barang` varchar(255) NOT NULL,
   `harga` int(11) NOT NULL,
-  `jumlah_barang` int(11) NOT NULL,
-  `tanggal_keluar` timestamp NOT NULL
+  `jumlah_barang` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `t_barang_masuk`
+-- Table structure for table `t_barang_masuk`
 --
 
 CREATE TABLE `t_barang_masuk` (
@@ -51,18 +50,10 @@ CREATE TABLE `t_barang_masuk` (
   `tanggal_masuk` timestamp NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
---
--- Dumping data untuk tabel `t_barang_masuk`
---
-
-INSERT INTO `t_barang_masuk` (`id`, `barcode`, `nama_barang`, `harga`, `jumlah_barang`, `tanggal_masuk`) VALUES
-(2, '365478095', 'Taro', 10000, 100, '2024-09-24 14:27:39'),
-(3, '580963258', 'Pota Bee', 5000, 29, '2024-09-30 02:31:27');
-
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `t_users`
+-- Table structure for table `t_users`
 --
 
 CREATE TABLE `t_users` (
@@ -72,54 +63,55 @@ CREATE TABLE `t_users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `t_users`
+-- Dumping data for table `t_users`
 --
 
 INSERT INTO `t_users` (`id`, `username`, `password`) VALUES
 (1, 'admin', '$2y$10$JOpSYI.MbyhD/PARsD8aQOISUq4aMw3YR66bzopfQOrPoydUoiCje'),
-(2, 'Ardiona', '$2y$10$qbTYdKr.dE/XFpS1TCIoruMWQ487umE84lTAWxoh.mqqWwU9fjQaG');
+(2, 'ardiona', '$2y$10$HhZdMFTsfbcPuZGWfmbVSOOT0eyJ8rU5ybJQaY7A8sIxp1pppbFfq');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `t_barang_keluar`
+-- Indexes for table `t_barang_keluar`
 --
 ALTER TABLE `t_barang_keluar`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `barcode` (`barcode`) USING BTREE;
+  ADD KEY `barcod_barang_keluar` (`barcode`);
 
 --
--- Indeks untuk tabel `t_barang_masuk`
+-- Indexes for table `t_barang_masuk`
 --
 ALTER TABLE `t_barang_masuk`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `barcod_barang_masuk` (`barcode`);
 
 --
--- Indeks untuk tabel `t_users`
+-- Indexes for table `t_users`
 --
 ALTER TABLE `t_users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `t_barang_keluar`
+-- AUTO_INCREMENT for table `t_barang_keluar`
 --
 ALTER TABLE `t_barang_keluar`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT untuk tabel `t_barang_masuk`
+-- AUTO_INCREMENT for table `t_barang_masuk`
 --
 ALTER TABLE `t_barang_masuk`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
--- AUTO_INCREMENT untuk tabel `t_users`
+-- AUTO_INCREMENT for table `t_users`
 --
 ALTER TABLE `t_users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
